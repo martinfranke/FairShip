e9f8c575e6966d9b9b2ea42647856cd20138ad76
martinfranke <mfranke@physik.hu-berlin.de>
2015-05-28 11:39:28 +0200

    new CosmicsGenerator

--- shipgen/CosmicsGenerator.cxx
+++ shipgen/CosmicsGenerator.cxx
@@ -1,6 +1,6 @@
 // -------------------------------------------------------------------
 // -----       CosmicsGenerator source file for SHiP             -----
-// -----       Version by 01.12.14  by Martin Franke             -----
+// -----       Version by 01.06.15  by Martin Franke             -----
 // -----       mailto: mfranke(at)physik.hu-berlin.de            -----
 // -------------------------------------------------------------------
 
@@ -12,35 +12,43 @@
 
 using namespace std;
 
-Bool_t CosmicsGenerator::Init(Float_t zmiddle){
+Bool_t CosmicsGenerator::Init(Float_t zmiddle, Bool_t largeMom){
 	//general
 	fRandomEngine = new Co3Rng();
 	TDatabasePDG* pdgBase = TDatabasePDG::Instance();
 	mass = pdgBase->GetParticle(13)->Mass(); // muons!
 
+	EVENTS = 400000; // #simulated events per "spill"
 	// coordinate system
 	xdist = 3000; // production area size [cm]
 	zdist = 9000; // production area size [cm]
 	yTop = 600; // box top layer [cm]-> also change weight3 accordingly when varying
+	xTop = 300; // box side layer [cm]-> also change weight3 accordingly when varying
+	zTop = 3650; // box length [cm]-> also change weight3 accordingly when varying
 	z0 = zmiddle; // relative coordinate system [cm] (Z_muonstation + (Z_veto - 2 * Z_Tub1))/2,... Z_veto <0 ! ->z0 = 716
-	cout<<"!!!!!!!!!!!!!!!!!! z0: "<<z0<<endl;
-
+	cout<<"z0: "<<z0<<endl;
+	
+	high = largeMom;
+	if (high) cout<<"Simulation for high momentum"<<endl;
+	else cout<<"Simulation for low momentum"<<endl;
+	
 	// weights
-	Float_t weight1 = 174*xdist*zdist/10000/500000; // expected #muons per spill/ #simulated events per spill 174*30*90/500000
-	Float_t weight2 = 1.0/0.0830872; // 1/(mean momentum weight), P_max-P_min/(3*174/2pi)
-	Float_t weight3 = 5.174808; // MC average of nTry/nEvents 5.174808003 +- 0.000024766
-	weight = weight1 * weight2 / weight3;
+	if (!high) { // momentum range 1 GeV - 100 GeV
+		weight1 = 123*xdist*zdist/EVENTS/10000; // expected #muons per spill/ #simulated events per spill: 123*30*90/500000
+		weight2 = TMath::Pi/3;
+	} 
+	else {
+		double I = fRandomEngine->fspectrumH->Integral(100,1000);
+		weight1 = 2*TMath::Pi()/3*I*xdist*zdist/EVENTS/10000; // expected #muons per spill/ #simulated events per spill: 123*30*90/500000
+		weight2 = 900/I; // 1/(mean momentum weight), P_max-P_min/(3*0.3044/2pi)
+	}
+	double weight3 = 4.834154338; // MC average of nTry/nEvents 4.834154338 +- 0.000079500												
+	weight = weight1 * weight2  / weight3;
 
 	// running
-	y = 2000; //all muons start 20m over beam axis
-	nTry =0;  nInside = 0;  nEvent = 0; nTest = 0; weighttest = 0; // book keeping
+	y = 1900; //all muons start 19m over beam axis
+	nInside = 0;  nEvent = 0; nTest = 0; weighttest = 0; // book keeping
 	return kTRUE;
-}
-
-Float_t CosmicsGenerator::getPweight(Float_t P){
-	// implementation of the cosmic muon spectrum at sea evel
-	if (P<100){return 22*TMath::Power(P,-0.6 -0.285*TMath::Log(P));}
-	else{return 1400*TMath::Power(P,-2.7)*(1/(1+P/115)+0.054/(1+P/850));}
 }
 
 // -----   Passing the event   ---------------------------------------------
@@ -49,40 +57,39 @@ Bool_t CosmicsGenerator::ReadEvent(FairPrimaryGenerator* cpg){
 
 	do{
 		// shower characteristics
-		multiplicity = fRandomEngine->randomMultiplicity();
-		Float_t showerPhi = fRandomEngine->Uniform(0,2*TMath::Pi());
-		Float_t showerTheta = fRandomEngine->randomTheta();
+		double phi = fRandomEngine->Uniform(0,2*TMath::Pi());
+		double theta = fRandomEngine->fTheta->GetRandom();
 
-		for(Int_t m = 0;m < multiplicity; m++){
-			// muon momentum and spectrum weight
-			Float_t P = fRandomEngine->Uniform(0.1,1000);
-			w = getPweight(P) * weight;
+		//momentum components
+		px = TMath::Sin(phi)*TMath::Sin(theta); 
+		pz = TMath::Cos(phi)*TMath::Sin(theta);
+		py = -TMath::Cos(theta);
 
-			// shower direction with small variations for individual muons
-			Float_t phi = showerPhi + TMath::Pi()*fRandomEngine->Uniform(-1,1)/180;
-			Float_t theta = showerTheta + TMath::Pi()*fRandomEngine->Uniform(-1,1)/180*2*TMath::Sin(showerTheta)*TMath::Cos(showerTheta);
+		// start position, area 1120 m^2
+		x = fRandomEngine->Uniform(-xdist/2,xdist/2);
+		z = fRandomEngine->Uniform(z0 - zdist/2, z0 + zdist/2);
 
-			//momentum components
-			px = TMath::Sin(phi)*TMath::Sin(theta)*P;
-			pz = TMath::Cos(phi)*TMath::Sin(theta)*P;
-			py = TMath::Cos(theta)*P;
-
-			// start position, area 1120 m^2
-			x = fRandomEngine->Uniform(-xdist/2,xdist/2);
-			z = fRandomEngine->Uniform(z0 - zdist/2, z0 + zdist/2);
-
-			// claim for flight through a box surrounding the detector
-			if((TMath::Abs(x-(y+yTop)*px/py) < 300 && TMath::Abs(z-z0-(y+yTop)*pz/py) < 3650) || (TMath::Abs(x-(y-yTop)*px/py) < 300 && TMath::Abs(z-z0-(y-yTop)*pz/py) <  3650)|| (TMath::Abs(y-(x+300)*py/px)<yTop && TMath::Abs(z-z0-(x+300)*pz/px)<3650) || (TMath::Abs(y-(x-300)*py/px)<yTop && TMath::Abs(z-z0-(x-300)*pz/px)<3650)){
-				//muon or anti-muon
-				if (fRandomEngine->Uniform(0,1) < 1.0/2.278){id = 13;}
-				else{id = -13;}
-				// transfer to Geant4
-				cpg->AddTrack(id,px,py,pz,x,y,z,-1,true,TMath::Sqrt(P*P+mass*mass),0,w);  // -1 = Mother ID, true = tracking, SQRT(x) = Energy, 0 = t
-				hit = 1; nInside++;
+		// claim for flight through a box surrounding the detector
+		if((abs(x-(y+yTop)*px/py) < xTop && abs(z-z0-(y+yTop)*pz/py) < zTop) || (abs(x-(y-yTop)*px/py) < xTop && abs(z-z0-(y-yTop)*pz/py) <  zTop)|| abs(y-(x+xTop)*py/px)<yTop && abs(z-z0-(x+xTop)*pz/px)<zTop || abs(y-(x-xTop)*py/px)<yTop && abs(z-z0-(x-xTop)*pz/px)<zTop){
+			
+			//muon or anti-muon
+			if (fRandomEngine->Uniform(0,1) < 1.0/2.278){id = 13;}
+			else{id = -13;}
+			
+			if (!high) { P = fRandomEngine->fSpectrumL(theta); w = weight;}
+			else { 
+				P = fRandomEngine->Uniform(100,1000);
+				w = weight * fRandomEngine->fspectrumH->Eval(P);
 			}
-			nTry++; weighttest += w;
+			px = px*P;
+			py = py*P;
+			pz = pz*P;
+					
+			// transfer to Geant4
+			cpg->AddTrack(id,px,py,pz,x,y,z,-1,true,TMath::Sqrt(P*P+mass*mass),0,w);  // -1 = Mother ID, true = tracking, SQRT(x) = Energy, 0 = t
+			hit = 1; nInside++;
 		}
-		nTest++;
+		weighttest += w;	nTest++;
 	}while(!hit);
 	nEvent++;
 	if (!nEvent%10000){cout<<nEvent/10000<<"10k events have been simulated"<<endl;}
--- shipgen/CosmicsGenerator.h
+++ shipgen/CosmicsGenerator.h
@@ -1,6 +1,6 @@
 // -------------------------------------------------------------------
 // -----       CosmicsGenerator source file for SHiP             -----
-// -----       Version by 11/28/14  by Martin Franke             -----
+// -----       Version by 01.06.15  by Martin Franke             -----
 // -----       mailto: mfranke(at)physik.hu-berlin.de            -----
 // -------------------------------------------------------------------
 
@@ -18,70 +18,70 @@ using namespace std;
 class FairPrimaryGenerator;
 
 class Co3Rng{
- public:
-	Co3Rng() {
-		rng = new TRandom3(gRandom->GetSeed());
-		fTheta = new TF1("f2","cos(x)*cos(x)",TMath::Pi()/2,TMath::Pi());
-		fMultipl = new TH1I("fMultipl", "fMultipl",20,1,20);
-		fMultipl->Fill(1,230000);
-		fMultipl->Fill(2,9000);
-		fMultipl->Fill(3,1800);
-		fMultipl->Fill(4,800);
-		fMultipl->Fill(5,420);
-		fMultipl->Fill(6,240);
-		fMultipl->Fill(7,170);
-		fMultipl->Fill(8,120);
-		fMultipl->Fill(9,90);
-		fMultipl->Fill(10,70);
-		fMultipl->Fill(11,53);
-		fMultipl->Fill(12,42);
-		fMultipl->Fill(13,34);
-		fMultipl->Fill(14,31);
-		fMultipl->Fill(15,28);
-		fMultipl->Fill(16,21);
-		fMultipl->Fill(17,19);
-		fMultipl->Fill(18,18);
-		fMultipl->Fill(19,17);
-		fMultipl->Fill(20,16);		
-	};
-	virtual ~Co3Rng() {delete rng; delete fTheta; delete fMultipl;};
-  
-	Float_t randomTheta() {return fTheta->GetRandom();};//!
-	Float_t Uniform(Float_t min, Float_t max){return rng->Uniform(min,max);};//!
-	Int_t randomMultiplicity(){return (Int_t)fMultipl->GetRandom();};//!
-  
- private:
-	TRandom3 *rng; //!
-	TF1 *fTheta;//!
-	TH1I *fMultipl;//!
+	public:
+	   Co3Rng() {
+			rng = new TRandom3(gRandom->GetSeed());
+			fTheta = new TF1("f2","cos(x)*cos(x)",0,1.3734); // theta_zenith up to 78.7deg
+			fTheta->SetNpx(10);
+			fSpectrumH = new TF1("f4","1400*TMath::Power(x,-2.7)*(1/(1+x/115)+0.054/(1+x/850))",100,1000); // momentum above 100GeV
+		};
+	   virtual ~Co3Rng() {delete rng; delete fTheta; delete fSpectrumH;};
+	   double Uniform(Float_t min, Float_t max){return rng->Uniform(min,max);};
+	   TF1 *fSpectrumH;
+	   TF1 *fTheta;
+	   double fSpectrumL(double theta); // momentum below 100GeV
+	private:
+	   TRandom3 *rng; //!
 };
+
+double Co3Rng::fSpectrumL(double theta){
+	//returns a random P between 1GeV and 100GeV taken from the 
+	// zenith angle dependend momentum distribution from 
+	// doi: 10.1016/j.nuclphysbps.2005.07.056.
+	// Here, the inverse of the function is computed and a random number 
+	// between 0 and 1 mapped to the interval [1, 100[ GeV
+	
+	theta = 180*theta/TMath::Pi(); // theta in degrees
+	double a = -0.8816/10000 /(1/theta  -0.1117/1000 * theta) - 0.1096 - 0.01966*TMath::Exp(-0.02040*theta);
+	double b = 0.4169/100 /(1/theta  -0.9891/10000 * theta) + 4.0395 - 4.3118*TMath::Exp(0.9235/1000*theta);
+
+	double gamma = sqrt(-TMath::Ln10()*a);
+	double offset = 0.5*(b  + 1/TMath::Ln10())/a;
+	double norm = TMath::Erf(gamma*(TMath::Log(100)+offset)) - TMath::Erf(gamma*offset);
+	
+	double r3 = rng->Uniform();
+	
+	return exp(TMath::ErfInverse(r3*norm+TMath::Erf(gamma*offset))/gamma-offset);
+}
 
 class CosmicsGenerator : public FairGenerator{
  public:
-  
-	/** constructor,destructor **/
+  	/** constructor,destructor **/
 	CosmicsGenerator(){};  
 	virtual ~CosmicsGenerator(){
 		delete fRandomEngine; 
-		cout<<nEvent<<" events with a mean multiplicity of " <<1.0*nTry/nTest<<" muons per event have been generated."<<endl;
-		cout<<"There is a total of "<<nInside<<" muons that passed close enough to the detector."<<endl;
-		cout<<"Including the given weight this corresponds to "<<weighttest/xdist/zdist*10000/174<<" spills (1 spill = 469800 real cosmic muons = 500000 simulated events)."<<endl;
+		cout<<nEvent<<" events have been generated."<<endl;
+		cout<<"There is a total of "<<nInside<<"/"<<nTest<<" muons that passed close enough to the detector."<<endl;
+		cout<<"Including the given weight this corresponds to ";
+		if (high) cout<<weighttest/xdist/zdist*10000/0.3044<<" spills (1 spill = "<<xdist*zdist*0.3044/10000;
+		else cout<<weighttest/xdist/zdist*10000/123<<" spills (1 spill = "<<xdist*zdist*123/10000;
+		cout<<" real cosmic muons = "<<EVENTS<<" simulated events)."<<endl;
 	};
   
 	/** public method ReadEvent **/
 	Bool_t ReadEvent(FairPrimaryGenerator*);  //!
 	//  virtual Bool_t Init(); //!
-	virtual Bool_t Init(Float_t zmiddle); //!
+	virtual Bool_t Init(Float_t zmiddle, Bool_t largeMom); //!
   
  private:
 	Co3Rng *fRandomEngine;//!
-	Float_t getPweight(Float_t P);//!
   
  protected:
-	Float_t px,py,pz,x,y,z,w, weighttest, weight, z0, mass, yTop, xdist, zdist;
-	Int_t id,nTry,nInside,nEvent,multiplicity,nTest;//!
-	
-	ClassDef(CosmicsGenerator,2);
+	double px,py,pz,x,y,z,w, weighttest, weight, z0, mass, yTop,xTop,zTop,xdist, zdist;
+	int id,nInside,nEvent,nTest,EVENTS;//!
+	Bool_t high;
+		
+	ClassDef(CosmicsGenerator,3);
 };
 
 #endif /* !PNDCoGENERATOR_H */
